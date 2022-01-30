f69ded73efd4afebbe9e6d23fe4ad63299314288
Juan Pablo Ocampo Rombolá <juanp.ocampo@hotmail.com>
2022-01-30 10:59:11 -0300

    config/mailer done

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,28 +1,27 @@
 const mailer = require("nodemailer");
 const mail = async (datos) => {
-  let {
-    nombreForm,
-    apellidoForm,
-    emailForm,
-    telForm,
-    provForm,
-    locForm,
-    comForm,
-    val,
-  } = datos;
-  let transporter = nodeMailer.createTransport({
+  let { mail, tel, name, last_name, company_name } = datos;
+  let transporter = mailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
     secure: true,
     auth: { user: "juanp.ocampo.22@gmail.com", pass: process.env.MAIL_PASS },
   });
   let info = await transporter.sendMail({
-    from: `${nombreForm} `,
-    to: `<${emailForm}>`,
-    subject: "Contacto MyWeb",
-    html: `<h1>contacto es: </h1><br>
-      Nombre: ${nombreForm}<br>
-      Email: ${emailForm}<br>
+    from: `juanp.ocampo.22@gmail.com `,
+    to: `<${mail}>`,
+    subject: `Respuesta a contacto de ${company_name}`,
+    html: `<h1>Muchas gracias por comunicarte conmigo: </h1><br>
+     <p>Hola ${last_name}, ${name}.</p>
+     <p>Muchas Gracias por comunicarte conmigo y tenerme en cuenta en tu búsqueda,
+     intentaré comunicarme contigo lo más pronto posible utilizando la información de contacto que suministraste</p>
+     <p>Mail: ${mail}</p>
+     <p>Teléfono: ${tel}</p>
+     <p>Saludos.</p>
+     <footer class="container-fluid p-5 bg-primary text-white text-center">
+      <p>Proyecto de WEB CV con soporte para contacto y autorespuesta a través de mail</p>
+      <p>Desarrollado por Juan Pablo Ocampo Rombolá < orJuanPablo ></p>
+     </footer>
       `,
   });
 };
